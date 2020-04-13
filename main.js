b60b58c4e418d477bc5b34a5a92cc40801f12ca9
Julia Zamaitat <j.zamaitat@gmail.com>
2020-04-13 16:07:32 +0200

    Finished simple routes.

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+const routeResponseMap = {
+  "/info": "<h1>Info Page</h1>",
+  "/contact": "<h1>Contact us</h1>",
+  "/about": "<h1>Learn more about us</h1>",
+  "/hello": "<h1>Say hello by emailing us here</h1>",
+  "/error": "<h1>Sorry the page you are looking for is not here</h1>",
+};
+
 const getJSONString = obj => {
   return JSON.stringify(obj, null, 2);
 };
@@ -22,9 +30,11 @@ app.on("request", (req, res) => {
   res.writeHead(httpStatus.OK, {
     "Content-Type": "text/html"
   });
-  let responseMessage = "<h1>This will show on the screen.</h1>";
-  res.end(responseMessage);
-
+  if (routeResponseMap[req.url]) {
+    setTimeout(() => res.end(routeResponseMap[req.url]), 2000);
+  } else {
+    res.end("<h1>Welcome!</h1>")
+  }
 });
 app.listen(port);
 console.log(`The server has started and is listening on port number: ${port}`);
