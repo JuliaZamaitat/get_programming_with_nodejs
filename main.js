5f2947b942d06acb0820b5cb6dbfe8838c6740e2
Julia Zamaitat <j.zamaitat@gmail.com>
2020-04-15 17:03:11 +0200

    Completed basic project with navigation.

--- main.js
+++ main.js
@@ -4,4 +4,22 @@ const port = 3000,
   router = require("./router"),
   contentTypes = require("./contentTypes"),
   utils = require("./utils");
-  
+
+
+router.get("/", (req,res) => {
+  res.writeHead(httpStatus.OK, contentTypes.html);
+  utils.getFile("views/index.html", res);
+});
+
+router.get("/courses.html", (req,res) => {
+  res.writeHead(httpStatus.OK, contentTypes.html);
+  utils.getFile("views/courses.html", res);
+});
+
+router.post("/", (req, res) => {
+  res.writeHead(httpStatus.OK, contentTypes.html);
+  utils.getFile("views/thanks.html", res);
+});
+
+http.createServer(router.handle).listen(port);
+console.log(`The server has started and is listening on port number: ${port}`);
