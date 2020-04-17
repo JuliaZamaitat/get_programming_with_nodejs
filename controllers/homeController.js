6e1b0120cc357987c62e8bcda4fa11c28fdacd60
Julia Zamaitat <j.zamaitat@gmail.com>
2020-04-17 20:53:07 +0200

    Added rerouting to a custom view.

---
+++ controllers/homeController.js
@@ -0,0 +1,3 @@
+exports.respondWithName = (req, res) => {
+  res.render("index"); //don't need specify folder or .js, express.js takes care of it as long as it is in the views folder
+};
--- main.js
+++ main.js
@@ -3,7 +3,11 @@ const port = 3000,
   app = express(),
   homeController = require("./controllers/homeController");
 
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);//sets port to enviromental port or localhost on 3000
+app.set("view engine", "ejs"); //informs about templating, sets view enging as ejs
+
+app.get("/name", homeController.respondWithName);
+
 
 app.listen(app.get("port"), () => {
   console.log(`Is listening on port: ${ app.get("port")}`);
