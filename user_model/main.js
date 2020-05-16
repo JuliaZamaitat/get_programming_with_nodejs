6a9c81cd316ba4fe45525cd2c26d95a53e3a4ff9
Julia Zamaitat <j.zamaitat@gmail.com>
2020-05-16 23:23:22 +0200

    Finished CRUD for users

--- user_model/main.js
+++ user_model/main.js
@@ -9,7 +9,8 @@ const express = require("express"),
   subscribersController = require("./controllers/subscribersController"),
   usersController = require("./controllers/usersController"),
   coursesController = require("./controllers/coursesController"),
-  router = express.Router();
+  router = express.Router(),
+  methodOverride = require("method-override");
 
   mongoose.connect(
     "mongodb://localhost:27017/confetti_cuisine",
@@ -29,10 +30,10 @@ app.use(express.json());
 app.use(layouts);
 app.use(express.static("public"));
 
+router.use(methodOverride("_method", {
+  methods: ["POST", "GET"]
+}));
 router.get("/", homeController.index);
-router.get("/contact", homeController.getSubscriptionPage);
-
-
 router.get("/subscribers", subscribersController.index, subscribersController.indexView);
 router.get("/courses", coursesController.index, coursesController.indexView);
 
@@ -40,6 +41,9 @@ router.post("/subscribe", subscribersController.saveSubscriber);
 router.get("/users", usersController.index, usersController.indexView);
 router.get("/users/new", usersController.new);
 router.get("/users/:id", usersController.show, usersController.showView);
+router.get("/users/:id/edit", usersController.edit);
+router.put("/users/:id/update", usersController.update, usersController.redirectView);
+router.delete("/users/:id/delete", usersController.delete, usersController.redirectView);
 router.post("/users/create", usersController.create, usersController.redirectView);
 
 app.use(errorController.pageNotFoundError);
