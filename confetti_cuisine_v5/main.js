e5ca31a3dd5556fa68c189cea125c5a952e0cfff
Julia Zamaitat <j.zamaitat@gmail.com>
2020-05-22 12:08:10 +0200

    Project setup & install of express-session, cookie-parser and connect-flash.

---
+++ confetti_cuisine_v5/main.js
@@ -0,0 +1,99 @@
+"use strict";
+
+const express = require("express"),
+  app = express(),
+  homeController = require("./controllers/homeController"),
+  errorController = require("./controllers/errorController"),
+  layouts = require("express-ejs-layouts"),
+  mongoose = require("mongoose"),
+  subscribersController = require("./controllers/subscribersController"),
+  usersController = require("./controllers/usersController"),
+  coursesController = require("./controllers/coursesController"),
+  router = express.Router(),
+  methodOverride = require("method-override"),
+  expressSession = require("express-session"),
+  cookieParser = require("cookie-parser"),
+  connectFlash = require("connect-flash");
+
+  mongoose.connect(
+    "mongodb://localhost:27017/confetti_cuisine",
+    {useNewUrlParser: true}
+  );
+
+
+app.set("view engine", "ejs");
+app.set("port", process.env.PORT || 3000);
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+
+app.use(express.json());
+app.use(layouts);
+app.use(express.static("public"));
+
+router.use(methodOverride("_method", {
+  methods: ["POST", "GET"]
+}));
+router.use(cookieParser("secret_passcode")); //as middleware
+router.use(expressSession({ //Configure express-session to use cookie-parser
+  secret: "secret_passcode",
+  cookie: {
+    maxAge: 4000000
+  },
+  resave: false,
+  saveUninitialized: false
+}));
+router.use(connectFlash()); //as middleware
+
+router.get("/", homeController.index);
+router.get("/contact", homeController.getSubscriptionPage);
+
+router.get("/subscribers", subscribersController.index, subscribersController.indexView);
+
+router.post(
+  "/subscribers/create",
+  subscribersController.create,
+  subscribersController.redirectView
+);
+router.get("/subscribers/:id/edit", subscribersController.edit);
+router.put(
+  "/subscribers/:id/update",
+  subscribersController.update,
+  subscribersController.redirectView
+);
+router.delete(
+  "/subscribers/:id/delete",
+  subscribersController.delete,
+  subscribersController.redirectView
+);
+router.get("/subscribers/:id", subscribersController.show, subscribersController.showView);
+
+router.get("/users", usersController.index, usersController.indexView);
+router.get("/users/new", usersController.new);
+router.get("/users/:id", usersController.show, usersController.showView);
+router.get("/users/:id/edit", usersController.edit);
+router.put("/users/:id/update", usersController.update, usersController.redirectView);
+router.delete("/users/:id/delete", usersController.delete, usersController.redirectView);
+router.post("/users/create", usersController.create, usersController.redirectView);
+
+router.get("/courses", coursesController.index, coursesController.indexView);
+router.get("/courses/new", coursesController.new);
+router.post("/courses/create", coursesController.create, coursesController.redirectView);
+router.get("/courses/:id/edit", coursesController.edit);
+router.put("/courses/:id/update", coursesController.update, coursesController.redirectView);
+router.delete("/courses/:id/delete", coursesController.delete, coursesController.redirectView);
+router.get("/courses/:id", coursesController.show, coursesController.showView);
+
+router.post("/subscribe", subscribersController.saveSubscriber);
+
+router.use(errorController.logErrors);
+router.use(errorController.respondNoResourceFound);
+router.use(errorController.respondInternalError);
+
+
+app.use("/", router);
+app.listen(app.get("port"), () => {
+  console.log(`Server running at http://localhost:${app.get("port")}`);
+});
