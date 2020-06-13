d8bfa215655c481dfd31abb307b30cc575a7afa3
Julia Zamaitat <j.zamaitat@gmail.com>
2020-06-13 20:12:48 +0200

    Added chat and persistence of chat messages with user information.

--- confetti_cuisine_v7/recipe_app/public/js/recipeApp.js
+++ confetti_cuisine_v7/recipe_app/public/js/recipeApp.js
@@ -1,4 +1,48 @@
 $(document).ready(() => {
+  const socket = io();
+
+  $("#chatForm").submit(() => {
+    let text = $("#chat-input").val(),
+      userName = $("#chat-user-name").val(),
+      userId = $("#chat-user-id").val();
+    socket.emit("message", {
+      content: text,
+      userName: userName,
+      userId: userId
+    });
+
+    $("#chat-input").val("");
+    return false;
+  });
+
+  socket.on("message", message => {
+    displayMessage(message);
+  });
+
+  let displayMessage = message => {
+		$("#chat").prepend(
+	    $("<li>").html(`
+				<strong class="message ${getCurrentUserClass(message.user)}">
+					${message.userName}
+				</strong>: ${message.content}
+			`)
+		);
+  };
+
+
+  let getCurrentUserClass = id => {
+    let userId = $("#chat-user-id").val();
+    return userId === id ? "current-user" : "";
+  };
+
+
+socket.on("load all messages", data => {
+  data.forEach(message => {
+    displayMessage(message);
+  });
+});
+
+
   $("#modal-button").click(() => {
     $(".modal-body").html("");
     $.get("/api/courses?apiToken=recipeT0k3n", (results = {}) => {
